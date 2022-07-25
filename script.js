f667c98cd4d902f7c02b7114e4699819867286ab
epick <none>
2022-07-25 17:37:45 -0300

    detalles de cards

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const TXT = "Welcome! :D"
 
-const imagenesCard = ["img/card1.jpg", "img/card2.png"]
+const tituloCards = ["Youtube", "Twitch"];
+const imagenesCard = ["img/card1.jpg", "img/card2.png"];
+const cardTxt = ["Mi canal donde subo boludeces y tutoriales :D ","Donde strimeo para variar"];
+const links = ["https://www.youtube.com/channel/UCZwxqFIJHoVyTn3CBO9DWzw/videos", "https://www.twitch.tv/epick_unsigned" ];
+
 
 document.querySelector("#contenido_txt").innerHTML = TXT;
 
-document.querySelector("#contenido_cards").innerHTML = `
-    <div class="card card-style">
-        <img src="${imagenesCard[0]}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+function insertarCardDefecto(numeroCard){
+    document.querySelector("#contenido_cards").innerHTML += `
+    <div class="col col-md-auto">
+        <div class="card card-style">
+            <img src="${imagenesCard[numeroCard]}" class="card-img-top card-img-modif" alt="...">
+            <div class="card-body">
+                <h5 class="card-title titulo-card">${tituloCards[numeroCard]}</h5>
+                <p class="card-text estilo-txt">
+                    ${cardTxt[numeroCard]}
+                </p>            
+                <a href="${links[numeroCard]}" class="btn btn-primary boton-links">Ir al canal</a>
+            </div>
         </div>
     </div>
-
 `
+}
+
+for(let i = 0 ; i < imagenesCard.length ; i++){
+    insertarCardDefecto(i);
+}
